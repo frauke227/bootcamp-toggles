6eeb87412ee38766a5af1041f48afec518873d76
Jens Keller <jens.keller@sap.com>
2022-12-30 15:54:26 +0800

    defaulting not needed anymore for cf

--- bulletinboard-reviews-nodejs/index-cf.js
+++ bulletinboard-reviews-nodejs/index-cf.js
@@ -1,26 +1,17 @@
 import cfenv from 'cfenv'
 import main from './lib/main.js'
 
-const appEnv = cfenv.getAppEnv({
-  vcapFile: 'vcap.json'
-})
-
-const { app: { port } } = appEnv
+const appEnv = cfenv.getAppEnv()
 
 const { uri: connectionString, sslcert: cert, sslrootcert: ca } = appEnv.getServiceCreds('bulletinboard-postgres')
 
-const { REVIEWS_ENDPOINT: endpoint = 'http://localhost:9090' } = process.env
-
 const config = {
   app: {
-    port
+    port: process.env.PORT
   },
   postgres: {
     connectionString,
     ssl: (cert && ca) ? { cert, ca } : false
-  },
-  reviews: {
-    endpoint
   }
 }
 
